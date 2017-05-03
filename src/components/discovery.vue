dcf1058f52ad68d87adecb4d6d5d1247cbdc8d96
lemontree <gonelove.guo@gmail.com>
2017-05-04 00:01:14 +0800

    list

--- src/components/discovery.vue
+++ src/components/discovery.vue
@@ -2,10 +2,17 @@
   <div class="recommend">
     <div class="recommend-header">
       <div class="scene-bar">
-
+        <ul>
+          <li>
+            <div>
+              <img src="" alt="">
+            </div>
+            <span></span>
+          </li>
+        </ul>
       </div>
       <div class="quick-list">
-
+        
       </div>
     </div>
   </div>
