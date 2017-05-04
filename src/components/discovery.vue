e27d321da28a7498791c8c015829346b052bd509
lemontree <gonelove.guo@gmail.com>
2017-05-04 23:57:29 +0800

    recommend-header

--- src/components/discovery.vue
+++ src/components/discovery.vue
@@ -2,17 +2,18 @@
   <div class="recommend">
     <div class="recommend-header">
       <div class="scene-bar">
-        <ul>
-          <li>
-            <div>
-              <img src="" alt="">
-            </div>
-            <span></span>
-          </li>
-        </ul>
+        <span class="scene-logo">场景音乐</span>
+        <span class="go-icon"></span>
       </div>
       <div class="quick-list">
-        
+        <ul class="quick-box">
+          <li class="quick-item">
+            <div class="image-box">
+              <img src="../assets/image/14671842274837.png" alt="">
+            </div>
+            <span class="item-title">跑步</span>
+          </li>
+        </ul>      
       </div>
     </div>
   </div>
